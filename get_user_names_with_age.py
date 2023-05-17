bb358ee639f99590b12f44b6b74263087cf8b8b7
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-17 12:32:03 +0500

    name with age

--- get_user_names_with_age.py
+++ get_user_names_with_age.py
@@ -8,4 +8,21 @@ def get_user_names_with_age(data:list, age:int) -> list:
     Returns:
         list: A list of users with the given age
     """
-    return 
+    a=''
+    for i in data:
+        if i['age']==age:
+            a=i['name']
+        
+    return a
+age=int(input('How old unknown object>>>>'))
+data = [
+  {
+    'name': 'John', 
+    'age': 27
+  }, 
+  {
+    'name': 'Mary', 
+    'age': 42
+  }
+]
+print(get_user_names_with_age(data,age))
