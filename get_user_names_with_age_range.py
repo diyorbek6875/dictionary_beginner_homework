17e6ead61d309fed1bc0ed67c0579403efdd6dfd
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-05-17 12:22:08 +0500

    name with age range

--- get_user_names_with_age_range.py
+++ get_user_names_with_age_range.py
@@ -9,4 +9,31 @@ def get_user_names_with_age_range(data:list, min_age:int, max_age:int) -> list:
     Returns:
         list: A list of users with the given age range
     """
-    return 
+    a=[]
+    for i in data:
+        if i['age']>min_age and i['age']<max_age:
+            a.append(i['name'])
+    
+
+    return a
+min_age=int(input('Minimal yoshni kiriting>>>>>'))
+max_age=int(input("Maxsimal yoshni kiriting>>>>>"))
+data = [
+  {
+    'name': 'John', 
+    'age': 20
+  }, 
+  {
+    'name': 'Mary', 
+    'age': 17
+  },
+  {
+    'name': 'Ban', 
+    'age': 23
+  },
+  {
+    'name': 'John2', 
+    'age': 27
+  }
+]
+print(get_user_names_with_age_range(data,min_age,max_age))
